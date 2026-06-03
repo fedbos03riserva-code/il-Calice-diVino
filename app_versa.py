1dfecb4c38b0f328a02498bcd3aca02021e3bdfb
fedbos03riserva-code <fedbos03riserva@gmail.com>
2026-06-03 17:35:19 +0200

    Update app_versa.py

--- app_versa.py
+++ app_versa.py
@@ -312,11 +312,7 @@ if st.button("🚀 TROVA I VINI PERFETTI"):
             """, unsafe_allow_html=True)
 
 # LOGICA DI FUNZIONAMENTO DEL SITO
-if st.button("🚀 TROVA I VINI PERFETTI", key="bottone_unico"): 
-    if not piatto:
-        st.warning("Ehi! Scrivi cosa stai mangiando (es: pasta al salmone o grigliata) per permettere all'applicazione di fare l'abbinamento!")
-    else:
-        st.write("---")
+
         
         # Identifico la macro-categoria del piatto inserito
         categoria = "generico"
